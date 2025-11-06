c288ef3d106f15d83b5f5d0070255f99f8284b5d
Raj123454321 <rajkesavan10122001@gmail.com>
2025-11-06 16:27:52 +0530

    corrected the import

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./slices/cartSlice";
+import cartSlice from "./slices/CartSlice";
 import AmountSlice from "./slices/AmountSlice";
 import QuantitySlice from "./slices/QuantitySlice";
 import ProductSlice from "./slices/ProductSlice";
